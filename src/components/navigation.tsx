da3fb245e59de86ed02082d9fd7976d04d097bb3
svenjasutter <svenja.sutter2@gmail.com>
2023-02-20 18:25:23 +0100

    add logo to navigation

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -3,17 +3,22 @@ import { component$ } from '@builder.io/qwik'
 export default component$(() => {
   return (
     <nav class="main-nav mx-auto">
-      <a href="/news" class="main-nav-item">
-        News
-      </a>
-      <a href="/games" class="main-nav-item">
-        Games
-      </a>
-      <a href="/join" class="main-nav-item">
-        Mitmachen
-      </a>
-      <a href="/gallery" class="main-nav-item hidden">
-        Gallerie
+      <div>
+        <a href="/news" class="main-nav-item">
+          News
+        </a>
+        <a href="/games" class="main-nav-item">
+          Games
+        </a>
+        <a href="/join" class="main-nav-item">
+          Mitmachen
+        </a>
+        <a href="/gallery" class="main-nav-item hidden">
+          Gallerie
+        </a>
+      </div>
+      <a href="#">
+        <img src="/logo_wide.png" class="logo h-14 justify-end" />
       </a>
     </nav>
   )
