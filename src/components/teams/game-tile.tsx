80602ef1b3ab291c35a51dc26a4708b1d8408b1a
Søren <imhofabian@gmail.com>
2023-03-24 00:19:26 +0100

    crudely connect teams page to temporary backend

--- src/components/teams/game-tile.tsx
+++ src/components/teams/game-tile.tsx
@@ -1,22 +1,29 @@
-import { component$ } from '@builder.io/qwik'
-
-interface GameTileProps {
-  src: string
-  href: string
-  alt: string
-}
+import { component$, useSignal, useTask$ } from '@builder.io/qwik'
+import type { Game } from '~/types'
 
 export function random() {
   return Math.round(Math.random())
 }
 
-export default component$(({ src, href, alt }: GameTileProps) => {
+export default component$(({ collectionId, id, image, name }: Game) => {
+  const data = useSignal({ src: '', url: '/error' })
+
+  useTask$(({ track }) => {
+    track(() => name)
+    data.value.url = name.toLowerCase().replace(' ', '-')
+  })
+
+  useTask$(({ track }) => {
+    track(() => image)
+    data.value.src = `http://159.69.196.31/api/files/${collectionId}/${id}/${image}`
+  })
+
   return (
     <section
       class={['games__tile', random() ? 'hover:-rotate-1' : 'hover:rotate-1']}
     >
-      <a href={href} class="games__tile-link">
-        <img src={src} alt={alt} class="games__tile-image" />
+      <a href={data.value.url} class="games__tile-link">
+        <img src={data.value.src} alt={name} class="games__tile-image" />
       </a>
     </section>
   )
