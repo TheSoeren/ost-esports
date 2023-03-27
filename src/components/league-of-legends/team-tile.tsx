1e6f1196bf92d49e2d274cbe4f21b49e64b13525
Søren <imhofabian@gmail.com>
2023-03-28 00:20:12 +0200

    late night commit

---
+++ src/components/league-of-legends/team-tile.tsx
@@ -0,0 +1,45 @@
+import { Resource, component$, useResource$ } from '@builder.io/qwik'
+import type { ListResult, Player, Team } from '~/types'
+import PlayerInfo from './player-info'
+
+export default component$(({ id, name }: Team) => {
+  const playerResource = useResource$<ListResult<Player>>(({ cleanup }) => {
+    const controller = new AbortController()
+    cleanup(() => controller.abort())
+
+    return getPlayers(controller, id)
+  })
+
+  return (
+    <div class="teams__tile">
+      <div>{name}</div>
+
+      <Resource
+        value={playerResource}
+        onPending={() => <>Loading...</>}
+        onRejected={(error) => <>Error: {error.message}</>}
+        onResolved={(players) => (
+          <div>
+            {players.items.map((player, index) => (
+              <PlayerInfo key={index} {...player} />
+            ))}
+          </div>
+        )}
+      />
+    </div>
+  )
+})
+
+export async function getPlayers(
+  controller?: AbortController,
+  teamId?: string
+): Promise<ListResult<Player>> {
+  const response = await fetch(
+    `http://159.69.196.31/api/collections/memberhip/records?filter=(team="${teamId}")`,
+    {
+      signal: controller?.signal,
+    }
+  )
+
+  return await response.json()
+}
