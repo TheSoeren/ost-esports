5a23635c00b61c8c80fe3f7b53635afa48d6a02c
Søren <imhofabian@gmail.com>
2023-07-04 14:37:16 +0200

    text fix

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -3,10 +3,7 @@ import { routeLoader$ } from '@builder.io/qwik-city'
 import type { ListResult } from 'pocketbase'
 import Footer from '~/components/footer'
 import Header from '~/components/header'
-import {
-  getGameSpecificData,
-  LEAGUE_OF_LEGENDS,
-} from '~/data/teams/team-tile-mapping'
+import { LEAGUE_OF_LEGENDS } from '~/data/teams/team-tile-mapping'
 import pb from '~/pocketbase'
 import type { Team } from '~/types'
 import type { PlTeamDetailed } from '~/types/primeleague'
@@ -27,9 +24,25 @@ export const useTeamData = routeLoader$<UseTeamFetchingResponse>(async () => {
     $cancelKey: LEAGUE_OF_LEGENDS,
   })
 
-  const gameSpecificData = await getGameSpecificData(teams, LEAGUE_OF_LEGENDS)
+  const gameSpecificData = async () => {
+    const registeredTeams = teams.items
+      .filter((team) => !!(team.gameSpecificData.plTeamId as number))
+      .map((team) => team.gameSpecificData.plTeamId as number)
 
-  return structuredClone({ teams, gameSpecificData })
+    const plTeamList = await Promise.all(
+      registeredTeams.map((plTeamId: number) =>
+        fetch(`https://www.primebot.me/api/teams/${plTeamId}`)
+      )
+    ).then((responses) => Promise.all(responses.map((res) => res.json())))
+
+    if (plTeamList.length) {
+      return plTeamList
+    }
+
+    return []
+  }
+
+  return structuredClone({ teams, gameSpecificData: await gameSpecificData() })
 })
 
 export default component$(() => {
