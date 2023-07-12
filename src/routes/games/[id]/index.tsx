55a8d38c72c23aaaa1a7484d0d05b78d12f7224d
Søren <imhofabian@gmail.com>
2023-07-12 22:24:50 +0200

    Team creation and editing Fixes #33

---
+++ src/routes/games/[id]/index.tsx
@@ -0,0 +1,57 @@
+import {
+  component$,
+  Resource,
+  useResource$,
+  useStyles$,
+} from '@builder.io/qwik'
+import { useLocation } from '@builder.io/qwik-city'
+import TeamTile from '~/components/teams/team-tile'
+import styles from '~/css/teams.css?inline'
+import type { ListResult, Team } from '~/types'
+import fetch from '~/ajax'
+
+export default component$(() => {
+  useStyles$(styles)
+
+  const { params } = useLocation()
+
+  const teamsResource = useResource$<ListResult<Team>>(({ cleanup }) => {
+    const controller = new AbortController()
+    cleanup(() => controller.abort())
+
+    return getTeams(controller, params.id)
+  })
+
+  return (
+    <article class="page-content">
+      <div class="teams__container">
+        <Resource
+          value={teamsResource}
+          onPending={() => <>Loading...</>}
+          onRejected={(error) => <>Error: {error.message}</>}
+          onResolved={(teams) => (
+            <>
+              {teams.items.map((team, index) => (
+                <TeamTile key={index} {...team} />
+              ))}
+            </>
+          )}
+        />
+      </div>
+    </article>
+  )
+})
+
+export async function getTeams(
+  controller?: AbortController,
+  gameId?: string
+): Promise<ListResult<Team>> {
+  const response = await fetch(
+    `/api/collections/teams/records?filter=(game="${gameId}")`,
+    {
+      signal: controller?.signal,
+    }
+  )
+
+  return response.json()
+}
---
+++ src/routes/games/index.tsx
@@ -0,0 +1,49 @@
+import {
+  component$,
+  Resource,
+  useResource$,
+  useStyles$,
+} from '@builder.io/qwik'
+import GameTile from '~/components/games/game-tile'
+import styles from '~/css/games.css?inline'
+import type { Game, ListResult } from '~/types'
+import fetch from '~/ajax'
+
+export default component$(() => {
+  useStyles$(styles)
+
+  const gamesResource = useResource$<ListResult<Game>>(({ cleanup }) => {
+    const controller = new AbortController()
+    cleanup(() => controller.abort())
+
+    // Fetch the data and return the promises.
+    return getGames(controller)
+  })
+
+  return (
+    <article class="page-content">
+      <Resource
+        value={gamesResource}
+        onPending={() => <>Loading...</>}
+        onRejected={(error) => <>Error: {error.message}</>}
+        onResolved={(games) => (
+          <div class="games__container">
+            {games.items.map((game, index) => (
+              <GameTile key={index} {...game} />
+            ))}
+          </div>
+        )}
+      />
+    </article>
+  )
+})
+
+export async function getGames(
+  controller?: AbortController
+): Promise<ListResult<Game>> {
+  const response = await fetch(`/api/collections/games/records`, {
+    signal: controller?.signal,
+  })
+
+  return response.json()
+}
